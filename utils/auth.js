e8e9f4789047183954090f01c6d5b3712b2baeb1
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-16 17:59:38 -0400

    added code to postRoute.js and added auth.js to utils

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+// from module 14.28 mini project
+
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
