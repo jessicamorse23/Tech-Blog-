fdd0a7346d3a21212690c58eac6c00137ddf0068
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-22 17:02:29 -0400

    added to login.js and helpers

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+const loginForm = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-input-login').value.trim();
+  const password = document.querySelector('#password-input-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+.querySelector('.login-form')
+.addEventListener('submit', loginFormHandler);
+
+  
