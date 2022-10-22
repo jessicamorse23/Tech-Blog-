0062939cc01b1d5a4c49af921bc4adae2e8bcd94
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-22 15:12:02 -0400

    fixed the errors. always remember to DROP THE NEW SCHEMA ETC.

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS techblog_db;
 CREATE DATABASE techblog_db;
 USE techblog_db;
 
--- SET FOREIGN_KEY_CHECKS = 0;
--- drop table if exists posts;
--- drop table if exists comments;
--- drop table if exists users;
--- SET FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 0;
+drop table if exists posts;
+drop table if exists comments;
+drop table if exists users;
+SET FOREIGN_KEY_CHECKS = 1;
