c38700faa13caa6e413c49c5fa11511be4a95c6f
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-22 12:39:21 -0400

    changed comment model

--- models/Comment.js
+++ models/Comment.js
@@ -7,37 +7,36 @@ class Comment extends Model {}
 
 Comment.init(
   {
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true,
     },
-
+    comment_description: {
+      type: DataTypes.STRING,
+    },
     post_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'post',
         key: 'id',
       },
-
-      comments: {
-        type: DataTypes.STRING
-      },
-      
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "user",
-          key: "id",
-        },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
       },
     },
   },
   {
     sequelize,
-    timestamps: true, 
-    underscored: true, 
-    modelName: "comment",
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
   }
 );
-
 module.exports = Comment;
